dd5c8b640c3ac357e7b4176335ab2725d3124c05
Ignas <zigizmas@gmail.com>
2019-01-03 16:33:29 +0200

    client components

--- src/components/clients/Clients.js
+++ src/components/clients/Clients.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import  { Link } from "react-router-dom";
-import Proptypes from 'prop-types';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
 import { firestoreConnect } from 'react-redux-firebase';
@@ -77,8 +77,8 @@ class Clients extends Component {
 }
 
 Clients.propTypes = {
-  firestore: Proptypes.object.isRequired,
-  clients: Proptypes.array
+  firestore: PropTypes.object.isRequired,
+  clients: PropTypes.array
 };
 
 export default compose(
